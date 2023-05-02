c3486718bdbbeef0fd7d7ab37fa4a3b318b03a0f
lionelsu <lionel_suicune@hotmail.com>
2023-05-02 19:00:18 -0400

    implemented tests for NotFound component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testes para o componente <NotFound.js />', () => {
+  it('Deve conter um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Deve mostrar uma imagem com a url correta', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByRole('img', { name: /Pikachu crying because the page requested was not found/i });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
